54d91fd3a69a4b7b07e49438c9f4691cd1bbe7ac
Scott Teschendorf <teschendsn11@gmail.com>
2014-11-18 15:09:38 -0500

    update project.clj

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -5,3 +5,12 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "I'm a little Teapot!"))
+
+(defn runPoll
+  "create a simple polling function"
+  [count]
+  (if (> count 50)
+    (do (println count)(println "done"))
+    (do (println count) (runPoll (inc count)))
+  ))
+
