500532bacc1d5154c9660696a407dbdccb4f5ac5
Scott Teschendorf <teschendsn11@gmail.com>
2014-11-19 20:05:39 -0500

    cha cha cha  changes!
    1# Please enter the commit message for your changes. Lines starting

--- project.clj
+++ project.clj
@@ -4,7 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
-[org.clojure/clojure "1.6.0"][org.clojure/data.json "0.2.5"]]
+                 [org.clojure/clojure "1.6.0"]
+                 [org.clojure/data.json "0.2.5"]
+                 [korma "0.4.0"]
+                 [org.clojure/java.jdbc "0.3.6"]
+                 [mysql/mysql-connector-java "5.1.6"]]
   :main ^:skip-aot clojure-noob.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -1,10 +1,8 @@
 (ns clojure-noob.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "I'm a little Teapot!"))
+(use '[korma.db :as kdb])
+(use '[korma.core :as kCore])
 
 (defn runPoll
   "create a simple polling function"
@@ -12,5 +10,20 @@
   (if (> count 50)
     (do (println count)(println "done"))
     (do (println count) (runPoll (inc count)))
-  ))
+    ))
 
+(defn getUserData
+  "dbTest"
+  []
+  (kdb/defdb db (mysql {:db "clojure_test"
+                  :user "clojure"
+                    :password "buckydorf"}))
+  
+  (kCore/defentity user)
+  (kCore/select user))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (def results (map #(str (:firstname %) " " (:lastname %)) (getUserData)))
+  (println (first results)))
